9dc272a281f1a76cd3f4b84afcfa574117bf77c2
Ta0u <weichuen8984@gmail.com>
2024-02-05 00:00:24 +0800

    added nav bar debugging required for top menu to appear

--- app/src/main/java/com/sp/mad_project/Homepage.java
+++ app/src/main/java/com/sp/mad_project/Homepage.java
@@ -1,19 +1,27 @@
 package com.sp.mad_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class Homepage extends AppCompatActivity {
+
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.homepage);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(navSelected);
 
         // Find Recipes Button
         Button findRecipesButton = findViewById(R.id.findRecipesButton);
@@ -26,6 +34,7 @@ public class Homepage extends AppCompatActivity {
             }
         });
 
+
         cookingTimerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,4 +44,22 @@ public class Homepage extends AppCompatActivity {
         });
 
     }
+
+
+    NavigationView.OnNavigationItemSelectedListener navSelected = new NavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            int id = item.getItemId();
+            if (id== R.id.sidenav_exit) {
+                finish();
+            } else if (id == R.id.sidenav_about) {
+                ;
+            } else if (id == R.id.sidenav_help) {
+                ;
+            } else if (id == R.id.sidenav_login) {
+                ;
+            }
+            return true;
+        }
+    };
 }
